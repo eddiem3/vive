b9bf0e074a753c5c9f6f9087a267749ebfd44b05
Eddie Massey III <eem33@case.edu>
2019-07-07 21:40:54 -0700

    motion detection in open loop, restarts heroku

--- vivecam.py
+++ vivecam.py
@@ -5,9 +5,7 @@ import re
 from picamera import PiCamera
 from time import sleep
 import datetime
-from google.cloud import storage
-from google.cloud import bigquery
-from google.cloud.bigquery.client import Client
+
 import os
 import smtplib
 
@@ -22,8 +20,10 @@ def turnOnScreen():
 
 
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
-
-    creds = '.google/googlecreds.json'
+    from google.cloud import storage
+    from google.cloud import bigquery
+    from google.cloud.bigquery.client import Client
+    creds = '/home/pi/developemnt/vive-gpio/googlecreds.json'
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = creds
     bq_client = Client()
 
@@ -32,10 +32,12 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
 
 
     """Uploads a file to the bucket."""
+    print("Connecting to Google Cloud  Stoarage")
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
 
+    print("Uploading file")
     blob.upload_from_filename(source_file_name)
     blob.make_public()
 
@@ -97,73 +99,20 @@ def recordVideoCv(duration):
     
     with open('videos.csv','a') as fd:
         fd.write(file)
-     
-
-def recordVideoPi():
-    #Get current date and time
-    day = datetime.datetime.now()
-    format = '%a %b %d %H %M %S %Y'
-
-    #Create output filename
-    filepath = '../vive-web/public/videos/'
-
-    exh = '.h264' #input extension from PiCamera
-    exm =  '.mp4' #output extension after ffmpeg
     
-    #filename  = re.sub('[^A-Za-z0-9]+', '', day.strftime(format)).lower() 
-    filename  = day.strftime(format)
-    input_file_name = filepath + filename + exh
-    output_file_name =  filepath + filename +exm
-
-    camera = PiCamera()
-    
-    #Take a picture
-    imagepath = filepath +filename + '.jpg'
-    camera.start_preview()
-    sleep(5)
-    camera.capture(imagepath)
-    camera.stop_preview()
-
-    sleep(0.1)
-
-    #Record video
-
-    camera.start_recording(input_file_name)
-    camera.rotation = 90
-    camera.start_preview()
-
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(23, GPIO.IN) #PIR1
-    GPIO.setup(24, GPIO.IN) #PIR2
-    pir1 =  GPIO.input(23)
-    pir2 = GPIO.input(24)
-
-    if(not pir1 and not pir2):
-        sleep(10)
-        camera.stop_preview()
-        sleep(10)
-        print("Converting to a useable format.....")
-        command = ["ffmpeg", "-f", "h264", "-i",  input_file_name , "-c:v", "copy", output_file_name]
-        subprocess.call(command)
-        print("Converstion completed. Cleaning up....")
-        subprocess.call(["rm", input_file_name])
-        print("Uploading to cloud......")
-        upload_blob('vivecam',imagepath, filename + '.jpg') #upload image
-        upload_blob('vivecam', output_file_name, filename + exm) #upload video
-        email(filename)
-        subprocess.call(["rm", input_file_name])
-        
+            
         
 def email(filename):
+
+    print("Sending email  notification")
     
-    
-    TO = 'eddiemasseyiii@gmail.com'
+    TO = 'amogha@carbonorigins.com'
     SUBJECT = 'Motion Detected'
-    TEXT = 'Motion detected. View at ' + 'https://storage.googleapis.com/vivecam/' +filename +'.mp4'
+    TEXT = 'Motion detected. View at ' + '<https://storage.googleapis.com/vivecam/' +filename +'.mp4>'
 
     # Gmail Sign In
-    gmail_sender = 'eddiemasseyiii@gmail.com'
-    gmail_passwd = 'christ8!isking'
+    gmail_sender = 'vivecam001@gmail.com'
+    gmail_passwd = 'M00nP!3YUM'
     
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
@@ -183,36 +132,142 @@ def email(filename):
 
     server.quit()
 
+turnOffScreen()
+print("Initalizing GPIO Pins")
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(23, GPIO.IN) #PIR1
+GPIO.setup(24, GPIO.IN) #PIR2
 
 
-
-
-
-
-
-recordVideoPi()
-
-
-#turnOffScreen()
-# GPIO.setmode(GPIO.BCM)
-
-# GPIO.setup(23, GPIO.IN) #PIR1
-# GPIO.setup(24, GPIO.IN) #PIR2
 
 # try:
-#    time.sleep(1) 
+#    time.sleep(1)
 #    while True:
-#        if GPIO.input(23) or GPIO.input(24):
-             #turnOnScreen()
-#            print("Motion Detected...")
-#            
-#            time.sleep(1)
-#            recordVideoPi()
-#            turnOffScreen()
-#            time.sleep(0.1)
+#        pir1 = GPIO.input(23)
+#        pir2 = GPIO.input(24)
+
+#        print(pir1,pir2)
 
 # except:
 #    GPIO.cleanup()
+
+
+try:
+    time.sleep(1)
+    while True:
+        pir1 = GPIO.input(23)
+        pir2 = GPIO.input(24)
+        print("In loop")
+        print(pir1,pir2)
+        sleep(1)
+        #if motion is detected
+        if pir1 == 1 or  pir2 == 1:
+            turnOnScreen()
+            print("Motion Detected...")
+
+            #Get current date and time
+            day = datetime.datetime.now()
+            format = '%a %b %d %H %M %S %Y'
+           
+            #Create output filename
+            filepath = '../vive-web/public/videos/'
+
+            exh = '.h264' #input extension from PiCamera
+            exm =  '.mp4' #output extension after ffmpeg
+            
+            #filename  = re.sub('[^A-Za-z0-9]+', '', day.strftime(format)).lower() 
+            filename  = day.strftime(format)
+            input_file_name = filepath + filename + exh
+            output_file_name =  filepath + filename +exm
+           
+            camera = PiCamera()
+           
+            #Take a picture
+            rotation = 270
+            imagepath = filepath +filename + '.jpg'
+            camera.rotation = rotation
+            camera.start_preview()
+            sleep(2)
+            camera.capture(imagepath)
+
+            #Record Video
+            camera.start_recording(input_file_name)
+            camera.rotation = rotation
+            sleep(10)
+
+        
+            print("About to end recording")
+            camera.stop_recording()
+            camera.stop_preview()
+            camera.close()
+            print("Finished recording. Converting video.....")
+            command = ["ffmpeg", "-f", "h264", "-i",  input_file_name , "-c:v", "copy", output_file_name]
+            subprocess.call(command)
+            print("Converstion completed. Cleaning up....")
+            subprocess.call(["rm", input_file_name])
+            print("Uploading to cloud......")
+            upload_blob('vivecam',imagepath, filename + '.jpg') #upload image
+            upload_blob('vivecam', output_file_name, filename + exm) #upload video
+            email(filename)
+            subprocess.call(["heroku","restart","-a", "vivecam"])
+            turnOffScreen()
+            
+            
+                
+except:
+    GPIO.cleanup()
+               
+           
+               
+          
+
+               
+       
+
+
+
+
+
+
+
+
+
+
+#recordVideoPi()
+#email('fakefilename.mp4')
+
+#turnOffScreen()
+
+
+
+
+# try:
+#    while True:
+#        if GIPO.input(23) == 1 or GPIO.input(24) == 1:
+#            print("One of the sensors is HIGH PIR ONE: ", pir1, "PIR TWO: ", pir2)
+           #if GIPO.input(23) == 0 and GPIO.input(24) == 0:
+           #    print("One of the sensors is LOW PIR ONE: ", pir1, "PIR TWO: ", pir2)
+
+     
+           
+       #     sleep(0.1)
+        
+
+           
+
+       #     if(not pir1 and not pir2):
+       #         print("PIR ONE AND TWO REPORT LOW")
+       #         print("PIR ONE: ", pir1, "PIR TWO: " , pir2)
+       #         camera.stop_recording()
+
+       #         subprocess.call(["rm", input_file_name])
+
+            
+       #         time.sleep(1)
+       #         turnOffScreen()
+       #         time.sleep(0.1)
+
 
 
 
